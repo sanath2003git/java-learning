81c261b3150fa18ce48fb2038ef8cd362d59f36d
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-03-15 21:13:19 +0530

    Create VariableExample.java
    Added Java variables and data types example

---
+++ VariableExample.java
@@ -0,0 +1,12 @@
+public class VariableExample{
+        public static void main(String[] args){
+        String name = "Sanath";
+        System.out.println("Name: "+name);
+        int age = 22;
+        System.out.println("Age: "+age);
+        double height = 5.8;
+        System.out.println("Height: "+height);
+        boolean isStudent = true;
+        System.out.println("Student: "+isStudent);
+    }
+}
