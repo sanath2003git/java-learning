2f20ba3c03699387b5095761c9ecbbb489cbafff
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-03-21 01:14:39 +0530

    Create CompareScores.java

---
+++ CompareScores.java
@@ -0,0 +1,7 @@
+public class CompareScores {
+     public static void main(String[] args) { 
+        int player1 = 95;
+        int player2 = 85;
+        System.out.println(player1 > player2);
+    }
+ }
