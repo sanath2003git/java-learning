bb3907ae6ccaa3924d5de76f58015a2cb27cea29
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-03-21 01:18:42 +0530

    Create Qualification.java

---
+++ Qualification.java
@@ -0,0 +1,7 @@
+public class Qualification {
+    public static void main(String[] args) { 
+        int score = 90;
+        boolean premium = true; 
+        System.out.println(score > 80 && premium); 
+    } 
+}
