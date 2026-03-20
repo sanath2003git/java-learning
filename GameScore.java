2e17e0a0f856871d5e487ba7ce458997a49dd649
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-03-21 01:10:57 +0530

    Create GameScore.java

---
+++ GameScore.java
@@ -0,0 +1,8 @@
+public class GameScore{
+    public static void main(String[] args){
+        int level1 = 50;
+        int level2 = 45;
+        int totalScore =level1 + level2;
+        System.out.println("Total Score: "+ totalScore);
+    }
+}
