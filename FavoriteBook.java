df45faa4897cd168ee84567b6598465142d3ffd2
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-03-21 01:23:38 +0530

    Create FavoriteBook.java

---
+++ FavoriteBook.java
@@ -0,0 +1,12 @@
+public class FavoriteBook {
+    public static void main(String[] args) {
+        String title = "Dtacula";
+        String author = "Bram Stoker";
+        int pages = 223;
+        double price = 19.99;
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Price: $ " + price);
+    }
+}
