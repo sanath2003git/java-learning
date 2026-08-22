e73de1cd0cd3660574cd872462378d577c0d69b7
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-08-22 14:08:05 +0530

    Create NameLength.java

---
+++ NameLength.java
@@ -0,0 +1,8 @@
+public class NameLength {
+    
+    public static void main(String[] args){
+        String name="Charlie";
+        int len=name.length();
+        System.out.println("your name has " + len + " characters.");
+    }
+}
