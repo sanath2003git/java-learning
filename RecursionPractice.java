e28b171cceb49c80b376e0e87c920f3fd79f4250
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-08-26 18:19:38 +0530

    Create RecursionPractice.java

---
+++ RecursionPractice.java
@@ -0,0 +1,15 @@
+public class RecursionPractice {
+    public static void countdown(int n){
+        if (n == 0) {
+            System.out.println("Done!");
+            return;
+        }
+        System.out.println(n);
+        countdown(n - 1);
+    }
+    public static void main(String[] args){
+
+        countdown(5);
+    }
+    
+}
